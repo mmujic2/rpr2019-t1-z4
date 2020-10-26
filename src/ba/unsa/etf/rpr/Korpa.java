19d4299e47ec0561671866007ca4bc33a08cab0f
mmujic2 <mmujic2@etf.unsa.ba>
2020-10-26 12:53:42 +0100

    final commit.

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] artikli = new Artikl[50];
+
+    public boolean dodajArtikl(Artikl a) {
+        for (int i = 0; i < 50; i++){
+            if (artikli[i] == null) {
+                artikli[i] = a;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a = null;
+        for (int i = 0; i < 50; i++){
+            if (artikli[i] != null && artikli[i].getKod().equals(kod)){
+                a = artikli[i];
+                artikli[i] = null;
+            }
+        }
+        return a;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for (int i = 0; i < 50; i++){
+            if (artikli[i] != null) suma = suma + artikli[i].getCijena();
+        }
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl[] artikli = new Artikl[1000];
+
+    public void dodajArtikl(Artikl a) {
+        for (int i = 0; i < 1000; i++){
+            if (this.artikli[i] == null) {
+                artikli[i] = a;
+                break;
+            }
+        }
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a = null;
+        for (int i = 0; i < 1000; i++){
+            if (artikli[i] != null && artikli[i].getKod().equals(kod)){
+                a = artikli[i];
+                artikli[i] = null;
+            }
+        }
+        return a;
+    }
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+
 }
